26048af562b92a0bdb27e3c1362cc9d84cfdb33f
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-06 18:05:51 -0300

    learn(ap01-js): encerra o codigo 1.4.1

---
+++ js/script.js
@@ -0,0 +1,70 @@
+//comparação
+//operadores == e ===
+// [] new ArrayList<Object>();
+// console.log([] == [])
+// console.log([] == false)
+// console.log(null == undefined)
+// console.log (null == null)
+// a = []
+// b = 2
+// b = "abc"
+// if(b == a){
+
+// }
+// console.log(0 == [0, 1])
+// console.log(true == "true")
+// console.log(true == "1")
+// console.log(true == 1)
+// console.log(1 === "1")
+// console.log(1 === 1)
+// console.log(2 == "2")
+// console.log(1 == 1)
+//coerção
+// const n1 = 2
+// const n2 = '3'
+// // coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// // coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+
+//sistema de tipos
+// let idade = 20
+// console.log(typeof(idade))
+// const nome = "Ana"
+// console.log(typeof(nome))
+// idade = "vinte"
+// console.log(typeof(idade))
+//declaração de variáveis
+//const, let e var
+//hoist, içar
+// var idade = 18
+// console.log("Oi, " + nome)
+// if(idade >= 18){
+//   var nome = "João"
+//   console.log("Sim, " + nome + ". Você pode dirigir.")  
+// }
+// console.log(`Até mais, ${nome}`)
+// int a;
+// //String a;
+// var linguagem = "Javascript"
+// console.log("Aprendendo " + linguagem)
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+// var c = 2
+// var nome = "José"
+// nome = "José da Silva"
+// console.log(nome)
+// let a = 2
+// let nome = "José"
+// console.log(a)
+// console.log(nome)
+// nome = "José da Silva"
+// console.log(nome)
+
+//String nome = "José"
+// const nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva'
