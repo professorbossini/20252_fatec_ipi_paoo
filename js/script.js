7247080518f626ee95c50a0370f32d067ae52271
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-13 16:30:01 -0300

    learn(ap01-js): encerra o codigo 1.6.3
    - lidamos com vetores
    - lidamos com funcoes
    - retomar lidando com closures

--- js/script.js
+++ js/script.js
@@ -1,3 +1,88 @@
+//vetores, parte 2
+// mapreduce
+// const valores = [1, 2, 3, 4]
+// const soma = valores.reduce((ac, v) => ac + v)
+// console.log(soma)
+
+// const nomes = ['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina']
+// nomes.
+// const todosComecamComA = nomes.every(n => n.startsWith('A'))
+// console.log(todosComecamComA)
+// const peloMenosUmComecaComA = nomes.some(n => n.startsWith('A'))
+// console.log(peloMenosUmComecaComA)
+// //dada a coleção a seguir, produzir outra coleção contendo o quadrado de cada número
+// //usando arrow functions e digitando a menor quantidade de caracteres possível
+// const numeros = [1, 2, 3] //[1, 4, 9]
+// console.log(numeros.map(n => n * n))
+//['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina'] -> ['A', 'A', 'R', 'A', 'C']
+// const res = nomes.map(function(nome){return nome[0]})
+// console.log(res)
+// const apenasComA = nomes.filter(nome => nome.startsWith('A'))
+// console.log(apenasComA)
+// //arrow functions
+//tem exatamente um parâmetro: parênteses podem ser omitidos
+// const dobro = n => 2 * n
+// console.log(dobro(2))
+//tem somente uma linha que produz um valor a ser devolvido: 
+// const dobro = (n) => {
+//   console.log('Calculando o dobro...') 
+//   return 2 * n
+// }
+// console.log(dobro(1))
+// const dobro = (n) => 2 * n
+// console.log(dobro(5))
+
+//tem somente uma linha: as chaves podem ser omitidas
+// const hello = () => console.log("Hello")
+// hello()
+// const hello = () => {
+//   console.log('Hello')
+// }
+// hello()
+
+// const triplo = function(n = 5){
+//   return 3 * n
+// }
+// console.log(triplo())
+// console.log(triplo(10))
+
+// const dobro = function (n){
+//   return 2 * n
+// }
+// console.log(dobro(6))
+///callable "chamável", que pode ser chamado
+
+// function soma(a, b){
+//   return a + b
+// }
+// const res = soma(2, 3)
+// console.log(res)
+// function hello(){
+//   console.log('Oi')
+// }
+// hello()
+// function hello(nome){
+//   console.log('Hello, ' + nome)
+// }
+// hello('Ana')
+// int somar(int a, int b){
+//   return a + b;
+// }
+
+//vetores
+// v2 = [2, "abc", true]
+// console.log(v2)
+// for(let i = 0; i < v2.length; i++){
+//   console.log(v2[i])  
+// }
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[10] = 2
+// console.log(v1.length)
+// v1[7] = "abc"
+
 //comparação
 //operadores == e ===
 // [] new ArrayList<Object>();
