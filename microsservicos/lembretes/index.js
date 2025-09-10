c4dad1d1fc0ada974edce1992070806ecdb196ff
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-10 18:13:12 -0300

    learn(ap01-js): encerra secao 4.3.11
    - retomar fazendo o post de lembretes
    - implementacao do observacoes ainda nao foi iniciada

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+//GET serve para obter coisas do servidor
+//POST serve para cadastrar itens novos
+//PUT serve para atualizar itens existentes
+//DELETE serve para apagar itens existentes
+
+// Um lembrete: {id: 1, texto: 'Fazer café'}
+/*
+  lembretes:
+  {
+    1: {id: 1, texto: 'Fazer café'},
+    2: {id: 2, texto: 'Ir à feira'} 
+  }
+*/
+let id = 1
+const lembretes = {}
+//definindo um endpoint que permite que lembretes sejam cadastrados
+//POST /lembretes (req, res) => {}
+app.post('/lembretes', function(req, res){
+
+})
+
+//definindo um endpoint que permite que a coleção de lembretes seja obtida
+//GET /lembretes (req, res) => {}
+app.get('/lembretes', (req, res) => {
+  res.json(lembretes)
+})
+
+const port = 4000
+app.listen(4000, () => console.log(`Lembretes. Porta ${port}.`))
